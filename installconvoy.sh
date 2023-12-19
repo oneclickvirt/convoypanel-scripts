0709eafd43729e6706cd2f5bc8a6d8fafba14ad1
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-12-19 03:53:04 +0000

    2023/12/19

--- installconvoy.sh
+++ installconvoy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # by spiritlhl
 # from https://github.com/oneclickvirt/convoypanel-scripts
-# 2023/04/04
+# 2023/12/19
 
 utf8_locale=$(locale -a 2>/dev/null | grep -i -m 1 -E "UTF-8|utf8")
 if [[ -z "$utf8_locale" ]]; then
@@ -22,36 +22,36 @@ _red() { echo -e "\033[31m\033[01m$@\033[0m"; }
 _green() { echo -e "\033[32m\033[01m$@\033[0m"; }
 _yellow() { echo -e "\033[33m\033[01m$@\033[0m"; }
 _blue() { echo -e "\033[36m\033[01m$@\033[0m"; }
-reading(){ read -rp "$(_green "$1")" "$2"; }
+reading() { read -rp "$(_green "$1")" "$2"; }
 REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'" "fedora" "arch")
 RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Fedora" "Arch")
 PACKAGE_UPDATE=("! apt-get update && apt-get --fix-broken install -y && apt-get update" "apt-get update" "yum -y update" "yum -y update" "yum -y update" "pacman -Sy")
 PACKAGE_INSTALL=("apt-get -y install" "apt-get -y install" "yum -y install" "yum -y install" "yum -y install" "pacman -Sy --noconfirm --needed")
 PACKAGE_REMOVE=("apt-get -y remove" "apt-get -y remove" "yum -y remove" "yum -y remove" "yum -y remove" "pacman -Rsc --noconfirm")
 PACKAGE_UNINSTALL=("apt-get -y autoremove" "apt-get -y autoremove" "yum -y autoremove" "yum -y autoremove" "yum -y autoremove" "")
-CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')" "$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)") 
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')" "$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)")
 SYS="${CMD[0]}"
 [[ -n $SYS ]] || exit 1
 for ((int = 0; int < ${#REGEX[@]}; int++)); do
-    if [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]]; then
-        SYSTEM="${RELEASE[int]}"
-        [[ -n $SYSTEM ]] && break
-    fi
+  if [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]]; then
+    SYSTEM="${RELEASE[int]}"
+    [[ -n $SYSTEM ]] && break
+  fi
 done
-apt-get --fix-broken install -y > /dev/null 2>&1
+apt-get --fix-broken install -y >/dev/null 2>&1
 curl -L https://raw.githubusercontent.com/oneclickvirt/convoypanel-scripts/main/build_swap.sh -o swap.sh && chmod +x swap.sh
 
-checkroot(){
-    _yellow "checking root"
-	  [[ $EUID -ne 0 ]] && _red "${RED}Please run this script with the root user!${PLAIN}" && exit 1
+checkroot() {
+  _yellow "checking root"
+  [[ $EUID -ne 0 ]] && _red "${RED}Please run this script with the root user!${PLAIN}" && exit 1
 }
 
-checkupdate(){
-	    _yellow "Updating package management sources"
-		  ${PACKAGE_UPDATE[int]} > /dev/null 2>&1
+checkupdate() {
+  _yellow "Updating package management sources"
+  ${PACKAGE_UPDATE[int]} >/dev/null 2>&1
 }
 
-check_ipv4(){
+check_ipv4() {
   API_NET=("ip.sb" "ipget.net" "ip.ping0.cc" "https://ip4.seeip.org" "https://api.my-ip.io/ip" "https://ipv4.icanhazip.com" "api.ipify.org")
   for p in "${API_NET[@]}"; do
     response=$(curl -s4m8 "$p")
@@ -66,107 +66,109 @@ check_ipv4(){
   export IPV4
 }
 
-checksystem(){
-    if [[ "$SYSTEM" == "Ubuntu" || "$SYSTEM" == "Debian" ]]; then
-        if [[ "$(lsb_release -rs)" == "20.04" || "$(lsb_release -rs)" == "22.04" || "$(lsb_release -rs)" == "11" ]]; then
-            return
-        fi
+checksystem() {
+  if [[ "$SYSTEM" == "Ubuntu" || "$SYSTEM" == "Debian" ]]; then
+    if [[ "$(lsb_release -rs)" == "20.04" || "$(lsb_release -rs)" == "22.04" || "$(lsb_release -rs)" == "11" ]]; then
+      return
     fi
-    _red "Error: Not support system, please check https://docs.convoypanel.com/guide/deployment"
-    exit 1
+  fi
+  _red "Error: Not support system, please check https://docs.convoypanel.com/guide/deployment"
+  exit 1
 }
 
-check_docker(){
+check_docker() {
   if ! systemctl is-active docker >/dev/null 2>&1; then
-      _green " \n Install docker \n " 
-      ${PACKAGE_INSTALL[int]} docker.io
+    _green " \n Install docker \n "
+    ${PACKAGE_INSTALL[int]} docker.io
   fi
 }
 
-check_jq(){
+check_jq() {
   if ! command -v jq >/dev/null 2>&1; then
-      _green " \n Install jq \n " 
-      ${PACKAGE_INSTALL[int]} jq
+    _green " \n Install jq \n "
+    ${PACKAGE_INSTALL[int]} jq
   fi
 }
 
-check_wget(){
+check_wget() {
   if ! command -v wget >/dev/null 2>&1; then
-      _green " \n Install wget \n " 
-      ${PACKAGE_INSTALL[int]} wget
+    _green " \n Install wget \n "
+    ${PACKAGE_INSTALL[int]} wget
   fi
 }
 
-check_curl(){
+check_curl() {
   if ! command -v curl >/dev/null 2>&1; then
-      _green " \n Install curl \n " 
-      ${PACKAGE_INSTALL[int]} curl
+    _green " \n Install curl \n "
+    ${PACKAGE_INSTALL[int]} curl
   fi
 }
 
-check_docker_compose(){
+check_docker_compose() {
   if ! command -v docker-compose >/dev/null 2>&1; then
-      _green "\n Install Docker Compose \n"
-      COMPOSE_URL=""
-      SYSTEM_ARCH=$(uname -m)
-      case $SYSTEM_ARCH in
-        "x86_64") COMPOSE_URL="https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64" ;;
-        "aarch64") COMPOSE_URL="https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-arm64" ;;
-        "armv6l") COMPOSE_URL="https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-armhf" ;;
-        "armv7l") COMPOSE_URL="https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-armhf" ;;
-        *) _red "\nArchitecture not supported for binary installation of Docker Compose.\n"; exit 1 ;;
-      esac
-      curl -SL $COMPOSE_URL -o /usr/local/bin/docker-compose
-      chmod +x /usr/local/bin/docker-compose
-   fi
+    _green "\n Install Docker Compose \n"
+    COMPOSE_URL=""
+    SYSTEM_ARCH=$(uname -m)
+    case $SYSTEM_ARCH in
+    "x86_64") COMPOSE_URL="https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64" ;;
+    "aarch64") COMPOSE_URL="https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-arm64" ;;
+    "armv6l") COMPOSE_URL="https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-armhf" ;;
+    "armv7l") COMPOSE_URL="https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-armhf" ;;
+    *)
+      _red "\nArchitecture not supported for binary installation of Docker Compose.\n"
+      exit 1
+      ;;
+    esac
+    curl -SL $COMPOSE_URL -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+  fi
 }
 
-checksystem2(){
-    # Check CPU core count
-    cpu_cores=$(grep -c ^processor /proc/cpuinfo)
-    if [[ $cpu_cores -lt 2 ]]; then
-        _red "Error: Minimum requirement not met. CPU core count should be at least 2."
-        exit 1
+checksystem2() {
+  # Check CPU core count
+  cpu_cores=$(grep -c ^processor /proc/cpuinfo)
+  if [[ $cpu_cores -lt 2 ]]; then
+    _red "Error: Minimum requirement not met. CPU core count should be at least 2."
+    exit 1
+  fi
+
+  # Check available memory + swap
+  available_memory=$(free -m | awk '/^Mem:/{print $2}')
+  available_swap=$(free -m | awk '/^Swap:/{print $2}')
+  if [[ $((available_memory + available_swap)) -lt 4000 ]]; then
+    # Calculate required swap size
+    required_swap=$((4000 - available_memory + available_swap))
+    _green "Build swap"
+    if [[ -n $available_swap ]]; then
+      # Remove existing swap
+      swapoff -a
+
+      # Create new swap
+      ./swap.sh ${required_swap}
+
+      # Turn on new swap
+      swapon -a
+    else
+      # Create new swap
+      ./swap.sh ${required_swap}
+
+      # Turn on new swap
+      swapon -a
     fi
+  fi
 
-    # Check available memory + swap
-    available_memory=$(free -m | awk '/^Mem:/{print $2}')
-    available_swap=$(free -m | awk '/^Swap:/{print $2}')
-    if [[ $((available_memory + available_swap)) -lt 4000 ]]; then
-        # Calculate required swap size
-        required_swap=$((4000 - available_memory + available_swap))
-        _green "Build swap"
-        if [[ -n $available_swap ]]; then
-            # Remove existing swap
-            swapoff -a
-
-            # Create new swap
-            ./swap.sh ${required_swap}
-
-            # Turn on new swap
-            swapon -a
-        else
-            # Create new swap
-            ./swap.sh ${required_swap}
-
-            # Turn on new swap
-            swapon -a
-        fi
+  # Check available disk space
+  disk_space=$(df -P / | awk '/^\/dev\//{print $4}')
+  if [[ $disk_space -lt 10000000 ]]; then
+    _red "Error: Minimum requirement not met. Available disk space should be at least 10 GiB."
+    if [ -n "$available_swap" ]; then
+      ./swap.sh "$available_swap"
+    else
+      ./swap.sh "0"
     fi
-
-    # Check available disk space
-    disk_space=$(df -P / | awk '/^\/dev\//{print $4}')
-    if [[ $disk_space -lt 10000000 ]]; then
-        _red "Error: Minimum requirement not met. Available disk space should be at least 10 GiB."
-        if [ -n "$available_swap" ]; then
-          ./swap.sh "$available_swap"
-        else
-          ./swap.sh "0"
-        fi
-        exit 1
-    fi
+    exit 1
+  fi
 }
-
 
 # reload_apparmor(){
 #   # ${PACKAGE_INSTALL[int]} apparmor-utils
@@ -182,25 +184,24 @@ checksystem2(){
 #   fi
 # }
 
-checkconvoy(){
-    pve_version=$(pveversion)
-    if [ $? -ne 0 ]; then
-        _red "PVE is not installed"
-        exit 1
-    else
-        _green "PVE version is: $pve_version"
-    fi
-    if [[ $pve_version == "7.2-7" ]]; then
-        convoy_version="v2.0.3-beta"
-    elif [[ $pve_version == "7.3-4" || $pve_version > "7.3-4" ]]; then
-        convoy_version="latest"
-    else
-        _red "Error: Not support Proxmox Versions, please check https://docs.convoypanel.com/guide/deployment"
-        exit 1
-    fi
-    _green "Convoy version: $convoy_version"
+checkconvoy() {
+  pve_version=$(pveversion)
+  if [ $? -ne 0 ]; then
+    _red "PVE is not installed"
+    exit 1
+  else
+    _green "PVE version is: $pve_version"
+  fi
+  if [[ $pve_version == "7.2-7" ]]; then
+    convoy_version="v2.0.3-beta"
+  elif [[ $pve_version == "7.3-4" || $pve_version > "7.3-4" ]]; then
+    convoy_version="latest"
+  else
+    _red "Error: Not support Proxmox Versions, please check https://docs.convoypanel.com/guide/deployment"
+    exit 1
+  fi
+  _green "Convoy version: $convoy_version"
 }
-
 
 checkroot
 checkupdate
@@ -215,6 +216,26 @@ check_docker_compose
 check_jq
 checkconvoy
 _green "All minimum requirements are met."
+if [ -f "/usr/local/bin/apparmor.txt" ]; then
+  if ! dpkg -s apparmor >/dev/null 2>&1; then
+    _green "AppArmor is being installed..."
+    apt-get install -y apparmor
+  fi
+  if [ $? -ne 0 ]; then
+    apt-get install -y apparmor --fix-missing
+  fi
+  if ! systemctl is-active --quiet apparmor.service; then
+    _green "Starting the AppArmor service..."
+    systemctl enable apparmor.service
+    systemctl start apparmor.service
+  fi
+  if ! lsmod | grep -q apparmor; then
+    _green "Loading AppArmor kernel module..."
+    modprobe apparmor
+  fi
+  echo "1" >"/usr/local/bin/apparmor.txt"
+  echo "Please execute reboot to reboot the system to load the kernel."
+fi
 if [ ! -d "/var/www/convoy" ]; then
   mkdir -p /var/www/convoy
 fi
@@ -249,7 +270,7 @@ _green "Now DB_USERNAME=$random_str2"
 _green "Now DB_PASSWORD=$random_str3"
 _green "Now DB_ROOT_PASSWORD=$random_str4"
 _green "Now REDIS_PASSWORD=$random_str5"
-docker-compose up -d
+docker compose up -d
 cd /
 curl -fsSL https://github.com/convoypanel/broker/releases/latest/download/broker.tar.gz | tar -xzv
 cd /var/www/convoy
